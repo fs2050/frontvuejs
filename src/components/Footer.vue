91b96ccae12b5d138c67f8cdbd77306aa26b7f7c
fs2050 <fabiolfs76@hotmail.com>
2021-11-06 12:29:19 -0300

    deploy frontend

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,17 @@
 <template>
-<p class="footer">Fabio Salles</p>
+<div class="card text-center">
+  <div class="card-header">
+    Larave 8 & VueJs
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Frontend com VueJs</h5>
+    <p class="card-text">API com Laravel 8</p>
+    <a href="#" class="btn btn-primary">https://myapil8.herokuapp.com/</a>
+  </div>
+  <div class="card-footer text-muted">
+    Fabio Salles
+  </div>
+</div>
 </template>
 
 <script>
